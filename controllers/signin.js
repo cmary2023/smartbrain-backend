3c1630453b0069963697c6c02e9e175e4515f904
Maria Corban <121573716+cmary2023@users.noreply.github.com>
2023-09-20 20:38:42 +0100

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,21 +1,21 @@
 const handleSignin = (req, res, db, bcrypt)=> {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json('incorrect form submission');
+    const { email, password } = req.body;// request comes from front-end
+    if (!email || !password) {// if email/password were kept empty and submitted (i.e. '')
+      return res.status(400).json('incorrect form submission');// sending the status is optional
     }
-    db.select('email', 'hash').from('login')
-      .where('email', '=', email)
+    db.select('email', 'hash').from('login')// Using postgres query (login is a table defined in our database => (login=> id, hash(encrypted password), email))
+      .where('email', '=', email)// conditional query to filter for requested email into the database
       .then(data => {
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        if (isValid) {
-          return db.select('*').from('users')
-            .where('email', '=', email)
-            .then(user => {
-              res.json(user[0])
+        const isValid = bcrypt.compareSync(password, data[0].hash);// using bcrypt to decode hash, (returns true on match)
+        if (isValid) {// On match condition
+          return db.select('*').from('users') // returning response to front-end as below
+            .where('email', '=', email)// email is always unique (so to filter valid user from the database)
+            .then(user => {// sending user details to the front-end (user details exist in users table =>  id(PrimaryKey),name,email,entries,joined)
+              res.json(user[0])// returning user[0] to make sure we are sending one_(filtered)_user and not the whole table
             })
-            .catch(err => res.status(400).json('unable to get user'))
+            .catch(err => res.status(400).json('unable to get user'))// if credentials were valid (as per this if statement), and receieved any error (something might be wrong with server)
         } else {
-          res.status(400).json('wrong credentials')
+          res.status(400).json('wrong credentials')// for invalid user credentials
         }
       })
       .catch(err => res.status(400).json('wrong credentials'))
