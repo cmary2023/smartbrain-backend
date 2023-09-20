51b18a5e2124c8d03dbe1788d233114c23b9d708
Maria Corban <121573716+cmary2023@users.noreply.github.com>
2023-09-20 20:35:43 +0100

    Update register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,18 +1,22 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-      return res.status(400).json('incorrect form submission');
+    const { email, name, password } = req.body; // request comes from front-end
+    if (!email || !name || !password) {// if name, email or password were kept empty and submitted (i.e. '')
+      return res.status(400).json('incorrect form submission');// sending the status(400) is optional
     }
-    const hash = bcrypt.hashSync(password);
-      db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
+     // (Using bcrypt Asynchronously. 
+  // To implement it Synchronously, here's the documentation 
+  // https://www.npmjs.com/package/bcrypt-nodejs
+    const hash = bcrypt.hashSync(password);// Asynchronous
+        // Store hash in your password DB.
+      db.transaction(trx => {// Transaction is done for propagation of one or more changes to the database.
+        trx.insert({// Creating an object to insert...
+          hash: hash, 
+          email: email  
         })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
+        .into('login')// into the login table (contains => id, hash(encrypted password), email ) | Now login table is updated
+        .returning('email') // returning for further operations...
+        .then(loginEmail => { // into the user table...
+          return trx('users')// (contains => id, name, email, entries, joined)
             .returning('*')
             .insert({
               // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
@@ -23,12 +27,12 @@ const handleRegister = (req, res, db, bcrypt) => {
               name: name,
               joined: new Date()
             })
-            .then(user => {
-              res.json(user[0]);
+            .then(user => {// sending response from the server to the front-end
+              res.json(user[0]);// user[0] to make sure we are sending one_(filtered)_user and not the whole table
             })
         })
-        .then(trx.commit)
-        .catch(trx.rollback)
+        .then(trx.commit)// incase transaction was successful, then commit the changes 
+        .catch(trx.rollback)  // user[0] to make sure we are sending one_(filtered)_user and not the whole table
       })
       .catch(err => res.status(400).json('unable to register'))
   }
